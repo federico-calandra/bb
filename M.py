5c19471db2a799acdc28179e7e94cc229e438984
federico <federico.calandra@ymail.com>
2021-10-02 01:18:52 +0200

    initial commit

---
+++ M.py
@@ -0,0 +1,125 @@
+class Muscolo:
+	def __init__(self, strComparto, strNome, lstCapi, intEngage = None):
+		self.comparto = strComparto
+		self.nome = strNome
+		self.capi = lstCapi
+		self.livello = intEngage
+		
+class Esercizio:
+	def __init__(self, strNome, lstMuscoli):
+		self.nome = strNome
+		self.muscoli = []
+		for m in lstMuscoli:
+			self.muscoli.append(m)
+
+comparti = ['spalle', 'torso', 'braccia', 'dorso', 'addome', 'gambe']
+
+## Funzione che data una stringa restituisce gli oggetti nella lista target i cui nomi la contengono
+def cerca(intMode, strExp):
+	res = [[], []]
+	if intMode == 0:	# restituisce i muscoli il cui nome contine strExp
+		for m in muscoli:
+			if strExp in m.nome:
+				res[0].append(m)
+				res[1].append(m.nome)
+	if intMode == 1:	# restituisce gli esercizi il cui nome contiene strExp
+		for e in esercizi:
+			if strExp in e.nome:
+				res[0].append(e)
+				res[1].append(e.nome)	
+	if intMode == 2:	# restituisce i muscoli del comparto strExp
+		for m in muscoli:
+			if strExp == m.comparto:
+				res[0].append(m)
+				res[1].append(m.nome)	
+	if intMode == 3:	# restituisce gli esercizi che allenano il muscolo strExp
+		for e in esercizi:
+			for m in e.muscoli:
+				if strExp in m.nome:
+					res[0].append(e)
+					res[1].append(e.nome)
+	if intMode == 4:	# restituisce gli esercizi che allenano il capo strExp
+		for e in esercizi:
+			for m in e.muscoli:
+				if m.nome == strExp[0]:
+					for c in m.capi:
+						if strExp[1] == c:
+							res[0].append(e)
+							res[1].append(e.nome)
+		
+	return res
+
+def creaMuscoli():
+	muscoli = []
+	muscoli.append(Muscolo('spalle', 'deltoide', ['anteriore', 'laterale', 'posteriore']))
+	muscoli.append(Muscolo('spalle', 'rotondo grande', []))
+	muscoli.append(Muscolo('torso', 'pettorale grande', ['clavicolare', 'sternale', 'addominale']))
+	muscoli.append(Muscolo('dorso', 'dorsale', ['vertebrale', 'scapolare', 'iliaco', 'costale']))
+	muscoli.append(Muscolo('dorso', 'trapezio', ['discendente', 'trasverso', 'ascendente']))
+	muscoli.append(Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve']))
+	muscoli.append(Muscolo('braccia', 'brachiale', []))
+	muscoli.append(Muscolo('braccia', 'brachioradiale', []))
+	muscoli.append(Muscolo('braccia', 'tricipite brachiale', ['lungo', 'laterale', 'mediale']))
+	muscoli.append(Muscolo('addome', 'obliquo', []))
+	muscoli.append(Muscolo('addome', 'retto addominale', []))
+	muscoli.append(Muscolo('gambe', 'gluteo grande', []))
+	muscoli.append(Muscolo('gambe', 'gastrocnemio', ['mediale', 'laterale']))
+	muscoli.append(Muscolo('gambe', 'quadricipite femorale', ['femorale', 'laterale', 'intermedio', 'mediale']))
+	muscoli.append(Muscolo('gambe', 'tensore fascia lata', []))
+	muscoli.append(Muscolo('gambe', 'bicipite femorale', ['lungo', 'breve']))
+	muscoli.append(Muscolo('gambe', 'semimembranoso', []))
+	muscoli.append(Muscolo('gambe', 'semitendinoso', []))
+	return muscoli
+
+def creaEsercizi():
+	esercizi = []
+	esercizi.append(Esercizio('affondo con manubri', [Muscolo('gambe', 'quadricipite', ['femorale', 'laterale'], 1), Muscolo('gambe', 'gluteo grande', [], 1)]))
+	esercizi.append(Esercizio('affondo alternato', [Muscolo('gambe', 'gluteo grande', [], 1), Muscolo('gambe', 'quadricipite', ['femorale', 'laterale', 'mediale'], 1)]))
+	esercizi.append(Esercizio('alzata frontale', [Muscolo('spalle', 'deltoide', ['anteriore'], 1), Muscolo('torso', 'pettorale grande', ['clavicolare'], 1)]))
+	esercizi.append(Esercizio('alzata frontale con bilanciere', [Muscolo('spalle', 'deltoide', ['anteriore'], 1), Muscolo('torso', 'pettorale grande', ['clavicolare'], 1)]))
+	esercizi.append(Esercizio('alzata laterale', [Muscolo('spalle', 'deltoide', ['laterale'], 1), Muscolo('dorso', 'trapezio', ['discendente'], 2)]))
+	esercizi.append(Esercizio('alzata di spalle', [Muscolo('dorso', 'trapezio', ['discendente'], 1)]))
+	esercizi.append(Esercizio('croce panca piana', [Muscolo('torso', 'pettorale grande', ['clavicolare', 'sternale', 'addominale'], 1)]))
+	esercizi.append(Esercizio('croce panca inclinata', [Muscolo('torso', 'pettorale grande', ['clavicolare'], 1), Muscolo('spalle', 'deltoide', ['anteriore'], 1)]))
+	esercizi.append(Esercizio('crunch addominale', [Muscolo('addome', 'retto addominale', [], 1), Muscolo('gambe', 'tensore fascia lata', [], 2), Muscolo('gambe', 'quadricipite', ['femorale'], 2)]))
+	esercizi.append(Esercizio('crunch addominale alternato', [Muscolo('addome', 'retto addominale', [], 1), Muscolo('addome', 'obliquo', [], 1), Muscolo('gambe', 'tensore fascia lata', [], 2), Muscolo('gambe', 'quadricipite', ['femorale'], 2)]))
+	esercizi.append(Esercizio('curl avambraccio, presa neutra', [Muscolo('braccia', 'brachioradiale', [], 1), Muscolo('braccia', 'brachiale', [], 2), Muscolo('braccia', 'bicipite', ['lungo', 'breve'], 2)]))
+	esercizi.append(Esercizio('curl avambraccio con rotazione polso, presa neutra', [Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 1), Muscolo('braccia', 'brachioradiale', [], 2), Muscolo('braccia', 'brachiale', [], 2), Muscolo('spalle', 'deltoide', ['anteriore'], 3), Muscolo('torso', 'pettorale grande', ['clavicolare'], 3)]))
+	esercizi.append(Esercizio('curl avambraccio, presa prona', [Muscolo('braccia', 'brachiale', [], 1), Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 2)]))
+	esercizi.append(Esercizio('curl avambraccio, presa supina', [Muscolo('braccia', 'brachiale', [], 1), Muscolo('braccia', 'brachioradiale', [], 2)]))
+	# approfondire diretto e inverso #
+	esercizi.append(Esercizio('dip', [Muscolo('torso', 'pettorale grande', ['addominale']), Muscolo('braccia', 'tricipite brachiale', ['lungo', 'laterale', 'mediale']), Muscolo('spalle', 'deltoide', ['anteriore'])]))
+	esercizi.append(Esercizio('dip inverso', [Muscolo('braccia', 'tricipite brachiale', ['lungo', 'laterale', 'mediale']), Muscolo('torso', 'pettorale grande', ['clavicolare', 'sternale', 'addominale']), Muscolo('spalle', 'deltoide', ['anteriore'])]))
+	esercizi.append(Esercizio('distensione panca piana', [Muscolo('torso', 'pettorale grande', ['clavicolare', 'sternale', 'addominale'], 1), Muscolo('braccia', 'tricipite', ['lungo', 'mediale'], 2), Muscolo('spalle', 'deltoide', ['anteriore'], 2)]))
+	esercizi.append(Esercizio('distensione panca inclinata', [Muscolo('torso', 'pettorale grande', ['clavicolare'], 1), Muscolo('braccia', 'tricipite brachiale', ['lungo', 'mediale'], 2), Muscolo('spalle', 'deltoide', ['anteriore'], 2)]))
+	esercizi.append(Esercizio('distensione panca reclinata', [Muscolo('torso', 'pettorale grande', ['addominale'], 1), Muscolo('braccia', 'tricipite brachiale', ['lungo', 'mediale'], 2), Muscolo('spalle', 'deltoide', ['anteriore'], 2)]))
+	esercizi.append(Esercizio('estensione avambraccio in alto', [Muscolo('braccia', 'tricipite brachiale', ['lungo', 'laterale'], 1)]))
+	esercizi.append(Esercizio('estensione avambraccio in alto, a due mani', [Muscolo('braccia', 'tricipite brachiale', ['lungo', 'laterale', 'mediale'], 1)]))
+	esercizi.append(Esercizio('flessione laterale busto', [Muscolo('addome', 'obliquo', [], 1), Muscolo('addome', 'retto addominale', [], 2)]))
+	esercizi.append(Esercizio('kick back', [Muscolo('braccia', 'tricipite brachiale', ['laterale', 'lungo'], 1)]))
+	esercizi.append(Esercizio('lat machine avanti, presa prona', [Muscolo('dorso', 'dorsale', ['vertebrale', 'scapolare'], 1), Muscolo('dorso', 'rotondo grande', [], 1), Muscolo('dorso', 'trapezio', ['trasversale', 'ascendente'], 2), Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 2), Muscolo('braccia', 'brachiale', [], 2), Muscolo('torso', 'pettorale', ['clavicolare', 'sternale', 'addominale'], 3)]))
+	esercizi.append(Esercizio('lat machine dietro', [Muscolo('dorso', 'dorsale', ['iliaco', 'costale'], 1), Muscolo('spalle', 'rotondo grande', [], 1), Muscolo('dorso', 'trapezio', ['ascendente'], 2), Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 2), Muscolo('braccia', 'brachiale', [], 2), Muscolo('braccia', 'brachioradiale', [], 2)]))
+	esercizi.append(Esercizio('lat machine a braccia tese', [Muscolo('dorso', 'dorsale', ['vertebrale', 'scapolare', 'iliaco', 'costale'], 1), Muscolo('spalle', 'rotondo grande', [], 2), Muscolo('braccia', 'tricipite brachiale', ['lungo'], 2)]))
+	esercizi.append(Esercizio('lat tricipite, presa prona', [Muscolo('braccia', 'tricipite', ['laterale', 'lungo', 'mediale'], 1)]))
+	esercizi.append(Esercizio('lat tricipite, presa prona, di spalle', [ Muscolo('braccia', 'tricipite brachiale', ['lungo'], 1)]))
+	esercizi.append(Esercizio('lat tricipite, presa supina', [Muscolo('braccia', 'tricipite brachiale', ['mediale'], 1)]))
+	esercizi.append(Esercizio('lento avanti', [Muscolo('spalle', 'deltoide', ['anteriore', 'laterale'], 1), Muscolo('torso', 'pettorale grande', ['clavicolare'], 1), Muscolo('dorso', 'trapezio', ['discendente', 'trasverso', 'ascendente'], 2), Muscolo('braccia', 'tricipite brachiale', ['lungo', 'mediale'], 2)]))
+	esercizi.append(Esercizio('lento dietro', [Muscolo('spalle', 'deltoide', ['laterale', 'posteriore'], 1), Muscolo('dorso', 'trapezio', ['discendente', 'trasverso', 'ascendente'], 2), Muscolo('braccia', 'tricipite brachiale', ['laterale', 'lungo', 'mediale'], 2)]))
+	esercizi.append(Esercizio('piegamento sulle braccia', [Muscolo('torso', 'grande pettorale', ['clavicolare', 'sternale', 'addominale'], 1), Muscolo('braccia', 'tricipite brachiale', ['laterale', 'lungo', 'mediale'], 1), Muscolo('spalle', 'deltoide', ['anteriore'], 2)]))
+	esercizi.append(Esercizio('piegamento sulle braccia, mani rialzate', [Muscolo('torso', 'grande pettorale', ['addominale'], 1), Muscolo('braccia', 'tricipite brachiale', ['laterale', 'lungo', 'mediale'], 1), Muscolo('spalle', 'deltoide', ['anteriore'], 2)]))
+	esercizi.append(Esercizio('piegamento sulle braccia, piedi rialzati', [Muscolo('torso', 'grande pettorale', ['clavicolare'], 1), Muscolo('braccia', 'tricipite brachiale', ['laterale', 'lungo', 'mediale'], 1), Muscolo('spalle', 'deltoide', ['anteriore'], 2)]))
+	esercizi.append(Esercizio('pullover manubrio', [Muscolo('torso', 'pettorale grande', ['clavicolare', 'sternale', 'addominale'], 1), Muscolo('braccia', 'tricipite', ['lungo'], 1), Muscolo('spalle', 'rotondo grande', [], 1), Muscolo('dorso', 'dorsale', ['vertebrale', 'scapolare', 'iliaco', 'costale'], 1)]))
+	# approfondire varie impugnature #
+	esercizi.append(Esercizio('rematore con bilanciere', [Muscolo('dorso', 'dorsale', ['vertebrale', 'scapolare', 'iliaco', 'costale'], 1), Muscolo('dorso', 'rotondo grande', [], 1), Muscolo('spalle', 'deltoide', ['posteriore'], 1), Muscolo('dorso', 'trapezio', ['discendente', 'trasversale', 'ascendente'], 1)]))
+	esercizi.append(Esercizio('rematore verticale', [Muscolo('dorso', 'trapezio', ['discendente', 'trasversale', 'ascendente'], 1), Muscolo('spalle', 'deltoide', ['anteriore', 'laterale', 'posteriore'], 1), Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 1), Muscolo('braccia', 'brachioradiale', [], 2)]))
+	esercizi.append(Esercizio('sollevamento busto da terra', [Muscolo('addome', 'retto addominale', [], 1), Muscolo('addome', 'obliquo', [], 2)]))
+	esercizi.append(Esercizio('sollevamento gambe da terra', [Muscolo('addome', 'retto addominale', [], 1), Muscolo('addome', 'obliquo', [], 2), Muscolo('gambe', 'tensore fascia lata', [], 3), Muscolo('gambe', 'quadricipite', ['femorale'], 3)]))
+	esercizi.append(Esercizio('squat', [Muscolo('gambe', 'quadricipite femorale', ['femorale', 'laterale', 'intermedio', 'mediale'], 1), Muscolo('gambe', 'gluteo grande', [], 1)]))
+	# approfondire differenze fra avanti e dietro #
+	esercizi.append(Esercizio('trazione sbarra avanti, presa prona', [Muscolo('dorso', 'dorsale', ['vertebrale', 'scapolare'], 1), Muscolo('dorso', 'rotondo grande', [], 1), Muscolo('dorso', 'trapezio', ['ascendente'], 1), Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 2), Muscolo('braccia', 'brachiale', [], 2), Muscolo('braccia', 'brachioradiale', [], 2), Muscolo('torso', 'pettorale grande', ['clavicolare', 'trasversale', 'addominale'], 3)]))
+	esercizi.append(Esercizio('trazione sbarra avanti, presa supina', [Muscolo('dorso', 'dorsale', ['vertebrale', 'scapolare', 'iliaco', 'costale'], 1), Muscolo('dorso', 'rotondo grande', [], 1), Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 1), Muscolo('braccia', 'brachiale', [], 1), Muscolo('braccia', 'brachioradiale', [], 1), Muscolo('dorso', 'trapezio', ['trasverale', 'ascendente'], 2), Muscolo('torso', 'pettorale grande', ['clavicolare', 'trasversale', 'addominale'], 3)]))
+	esercizi.append(Esercizio('trazione sbarra dietro', [Muscolo('dorso', 'dorsale', ['iliaco', 'costale'], 1), Muscolo('spalle', 'rotondo grande', [], 1), Muscolo('dorso', 'trapezio', ['ascendente'], 1), Muscolo('braccia', 'bicipite brachiale', ['lungo', 'breve'], 2), Muscolo('braccia', 'brachiale', [], 2), Muscolo('braccia', 'brachioradiale', [], 2), Muscolo('torso', 'pettorale grande', ['clavicolare', 'trasversale', 'addominale'], 3)]))
+	return esercizi
+
+muscoli = creaMuscoli()
+esercizi = creaEsercizi()
---
+++ bb.py
@@ -0,0 +1,262 @@
+#! /usr/bin/env python3
+
+# Versione 2.1
+
+from PyQt5 import QtCore, QtWidgets, uic
+from datetime import *
+from os import mknod, listdir
+import M
+
+## Variabili usate nel programma
+initTabIndex = 0
+saveFileExt = '.ses'
+
+## Classe per carica il file .ui che definisce la finestra
+class Ui(QtWidgets.QWidget):
+	def __init__(self, ui_file):
+		super().__init__()
+		uic.loadUi(ui_file, self)
+
+## Classe che espande QListWidgetItem implementando livelli di gerarchia 
+class QListItem(QtWidgets.QListWidgetItem):
+	def __init__(self, strNome, widget=None, intLivello=None, strParent=None):
+		self.livello = intLivello
+		self.parent = strParent
+		if self.livello == 0:	# l'item indica un comparto
+			super().__init__(strNome, widget)
+			self.setFlags(self.flags() & ~QtCore.Qt.ItemIsEnabled)
+			
+		if self.livello == 1:	# l'item indica un muscolo
+			super().__init__(strNome, widget)
+			
+		if self.livello == 2:	# l'item indica un capo muscolare
+			super().__init__(strNome, widget)
+		
+## Funzione che inizializza la finestra window, e se è una dialog la visualizza anche
+def initializeWindow(window):
+	if window == window1:
+		# inizializzo tab1
+		for comparto in M.comparti:	# aggiungo i comparti
+			QListItem(':: '+comparto.capitalize()+' ::', window1.tab1_listMuscoli, 0)
+			for muscolo in M.muscoli:	# aggiungo i muscoli
+				if muscolo.comparto == comparto:
+					if muscolo.capi == []:
+						QListItem('    '+muscolo.nome.capitalize(), window1.tab1_listMuscoli, 1)
+					else:
+						QListItem('    '+muscolo.nome.capitalize(), window1.tab1_listMuscoli, 1)
+						for capo in muscolo.capi:
+							QListItem('      - '+capo, window1.tab1_listMuscoli, 2, muscolo.nome)						
+						
+		window.tab1_listMuscoli.setCurrentRow(1)
+		refreshList(window.tab1_listEsercizi, window.tab1_listMuscoli)	# preparo listEsercizi
+		
+		# ~ #inizializzo tab2
+		for obj in M.esercizi:	# aggiungo gli esercizi a listEsercizi
+			window.tab2_listEsercizi.addItem(obj.nome.capitalize())
+		window.tab2_listEsercizi.setCurrentRow(0)	# seleziono prima riga di listEsercizi
+				
+		refreshList(window.tab2_listMuscoli, window.tab2_listEsercizi)	# preparo listMuscoli
+		
+		window.tabWidget.setCurrentIndex(initTabIndex)	# imposto quale tab è visualizzata
+		window.show()
+	
+	if window == window2:
+		window.tableSessione.setColumnWidth(0, 330)
+		window.tableSessione.setColumnWidth(1, 30)
+		window.tableSessione.setColumnWidth(2, 30)
+		window.tableSessione.setColumnWidth(3, 100)
+		window.tableSessione.setRowCount(0)
+	
+	if window == dialogSalva:
+		window.tboxNome.clear()
+		window.show()
+		
+	if window == dialogCarica:
+		if window.isHidden() == False: return
+		dialogCarica.listWidget.clear()
+		#considero i file che hanno estensione .ses nella cartella attuale e li aggiungo a listWidget
+		ls = listdir()
+		for f in ls:
+			if f[-3:len(f)] == 'ses':
+				window.listWidget.addItem(f)
+		window.listWidget.setCurrentRow(0)
+		window.show()
+		
+	return window
+		
+## Funzione che fa popola la lista slave secondo i risultati di ricerca
+def refreshList(slave, master):
+	slave.clear()
+	sel = master.currentItem()
+	c1 = []
+	c2 = []
+	c3 = []
+	if master == window1.tab1_listMuscoli:	# usata su tab1 per gli esercizi che allenano un muscolo
+		if sel.parent is None:	# muscolo con 1 capo
+			res = M.cerca(3, sel.text().lstrip(' -').casefold())
+			for e in res[0]:
+				for m in e.muscoli: 
+					if m.nome == sel.text().lstrip(' -').casefold():
+						if m.livello == 1: c1.append(QListItem(e.nome.capitalize(), intLivello=1))
+						if m.livello == 2: c2.append(QListItem(e.nome.capitalize(), intLivello=1))
+						if m.livello == 3: c3.append(QListItem(e.nome.capitalize(), intLivello=1))
+			QListItem('Primari', slave, 0)
+			for item in c1: slave.addItem(item)
+			QListItem('Secondari', slave, 0)
+			for item in c2: slave.addItem(item)
+			QListItem('Ausiliari', slave, 0)
+			for item in c3: slave.addItem(item)
+		elif sel.parent is not None:	# muscolo con più capi
+			res = M.cerca(4, [sel.parent, sel.text().lstrip(' -').casefold()])
+			for e in res[0]:
+				for m in e.muscoli:
+					if m.nome == sel.parent and sel.text().lstrip(' -').casefold() in m. capi:
+						if m.livello == 1: c1.append(QListItem(e.nome.capitalize(), intLivello=1))
+						if m.livello == 2: c2.append(QListItem(e.nome.capitalize(), intLivello=1))
+						if m.livello == 3: c3.append(QListItem(e.nome.capitalize(), intLivello=1))
+			QListItem('Primari', slave, 0)
+			for item in c1: slave.addItem(item)
+			QListItem('Secondari', slave, 0)
+			for item in c2: slave.addItem(item)
+			QListItem('Ausiliari', slave, 0)
+			for item in c3: slave.addItem(item)
+					
+	if master == window1.tab2_listEsercizi:	# usata su tab2 per i muscoli allenati da un esercizio
+		res = M.cerca(1, sel.text().casefold())[0][0]	# in questo modo recupero l'oggetto esercizio
+		c1 = []
+		c2 = []
+		c3 = []
+		# questo forse può essere migliorato #
+		for m in res.muscoli:	# metto ogni muscolo nella categoria in base al livello di coinvolgimento
+			if m.livello == 1:
+				if m.capi == []: c1.append(QListItem(m.nome.capitalize(), intLivello=1))
+				else:
+					for c in m.capi: c1.append(QListItem(m.nome.capitalize()+' '+c, intLivello=1))
+			if m.livello == 2:
+				if m.capi == []: c2.append(QListItem(m.nome.capitalize(), intLivello=1))
+				else:
+					for c in m.capi: c2.append(QListItem(m.nome.capitalize()+' '+c, intLivello=1))
+			if m.livello == 3:
+				if m.capi == []: c3.append(QListItem(m.nome.capitalize(), intLivello=1))
+				else:
+					for c in m.capi: c3.append(QListItem(m.nome.capitalize()+' '+c, intLivello=1))
+		QListItem('Primari', slave, 0)
+		for item in c1: slave.addItem(item)
+		QListItem('Secondari', slave, 0)
+		for item in c2: slave.addItem(item)
+		QListItem('Ausiliari', slave, 0)
+		for item in c3: slave.addItem(item)
+		# fino a qua ma come?#
+	
+	slave.setCurrentRow(1)
+	return slave
+
+## Funzione che popola window2.tableSessione secondo elementi da listSessione o da file
+def buildSessione(ses = None):
+	initializeWindow(window2)
+
+	if ses is None:	# costruisco la tabella a partire da listSessione
+		if window1.tab1_listSessione.count() == 0: return
+		for i in range(window1.tab1_listSessione.count()):
+			item = window1.tab1_listSessione.item(i)
+			window2.tableSessione.insertRow(i)
+			window2.tableSessione.setItem(i, 0, QtWidgets.QTableWidgetItem(window1.tab1_listSessione.item(i).text()))
+		window2.tableSessione.setCurrentCell(0, 0)
+		
+	elif ses is not None:	# costruisco la tabella per invocazione da caricaSessione
+		for i in range(len(ses)):
+			window2.tableSessione.insertRow(window2.tableSessione.rowCount())
+			for j in range(window2.tableSessione.columnCount()):
+				window2.tableSessione.setItem(i, j, QtWidgets.QTableWidgetItem(ses[i][j]))
+	
+	dialogCarica.hide()
+	window2.show()
+	return window2.tableSessione
+	
+## Funzione che salva tableSessione su un file .ses, in forma CSV
+def salvaSessione(strNome):
+	t = datetime.today()
+	path = str(t.year) + '-' + str(t.month) + '-'+str(t.day)
+	if strNome == '': path = path + saveFileExt
+	else: path = path + '_' + str(strNome) + saveFileExt
+	
+	try : mknod(path)
+	except FileExistsError: print('Errore: file già esistente')	# TO-DO implementare dialog
+	with open(path, 'w') as f:
+		for r in range(window2.tableSessione.rowCount()):
+			for c in range(window2.tableSessione.columnCount()):
+				try: f.write(window2.tableSessione.item(r, c).text() + ';')
+				except: f.write(';')
+		f.write('\n')
+	
+	dialogSalva.close()
+	return 0
+
+## Funzione che genera una list i cui elementi sono altre list. Gli elementi di queste ultime descrivono la sessione da caricare
+def caricaSessione(strNome):
+	with open(strNome, 'r') as f:
+		lines = f.readlines()
+	ses = []
+	var = []
+	
+	for l in lines:
+		for i in l.split(';'):
+			if i != '\n':
+				var.append(i)
+		ses.append(var)
+		var = []
+	
+	buildSessione(ses)
+	return ses
+
+## Funzione che gestisce i pulsanti +, -, Up e Dn
+def tbHandler(intIndex, item = None):
+	if intIndex == 0:	# aggiungi
+		if item is None: return	# in questo modo evito l'eccezione se non c'è nessun elemento selezionato
+		window1.tab1_listSessione.addItem(item.text())
+		window1.tab1_listSessione.setCurrentRow(window1.tab1_listSessione.count()-1)
+		
+	if intIndex == 1:	# rimuovi
+		window1.tab1_listSessione.takeItem(item)
+		
+	if intIndex == 2:	# sposta su
+		if item == 0: return
+		window1.tab1_listSessione.insertItem(item-1, window1.tab1_listSessione.takeItem(item))
+		window1.tab1_listSessione.setCurrentRow(item-1)
+		
+	if intIndex == 3:	#sposta giù
+		if item == window1.tab1_listSessione.count()-1: return
+		window1.tab1_listSessione.insertItem(item+1, window1.tab1_listSessione.takeItem(item))
+		window1.tab1_listSessione.setCurrentRow(item+1)
+
+	return 0
+
+
+if __name__ == "__main__":
+	app = QtWidgets.QApplication([])	# necessario per mostrare le finestre
+	window1 = Ui('window1.ui')
+	window2 = Ui('window2.ui')
+	dialogSalva = Ui('dialogSalva.ui')
+	dialogCarica = Ui('dialogCarica.ui')
+	
+	## Inizializza window1
+	initializeWindow(window1)
+	
+	## Connettori
+	window1.tab1_listMuscoli.currentItemChanged.connect(lambda: refreshList(window1.tab1_listEsercizi, window1.tab1_listMuscoli))
+	window1.tab2_listEsercizi.currentItemChanged.connect(lambda: refreshList(window1.tab2_listMuscoli, window1.tab2_listEsercizi))
+	
+	window1.tbPlus.clicked.connect(lambda: tbHandler(0, window1.tab1_listEsercizi.currentItem()))
+	window1.tbMinus.clicked.connect(lambda: tbHandler(1, window1.tab1_listSessione.currentRow()))
+	window1.tbUp.clicked.connect(lambda: tbHandler(2, window1.tab1_listSessione.currentRow()))
+	window1.tbDn.clicked.connect(lambda: tbHandler(3, window1.tab1_listSessione.currentRow()))
+	
+	window1.pbOk.clicked.connect(lambda: buildSessione())
+	window1.pbCarica.clicked.connect(lambda: initializeWindow(dialogCarica))
+	
+	window2.pbSalva.clicked.connect(lambda: initializeWindow(dialogSalva))
+	
+	dialogSalva.pbSalva.clicked.connect(lambda: salvaSessione(dialogSalva.tboxNome.text()))
+	dialogCarica.pbCarica.clicked.connect(lambda: caricaSessione(dialogCarica.listWidget.currentItem().text()))
+
+input()
